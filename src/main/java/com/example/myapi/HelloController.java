07f1369fb76713fed397d9aa3d1ed02beb14c88c
XieChen-GB <xieche@gmail.com>
2026-05-28 20:26:35 +0900

    Spring Boot：返回List的API

--- src/main/java/com/example/myapi/HelloController.java
+++ src/main/java/com/example/myapi/HelloController.java
@@ -3,6 +3,9 @@ package com.example.myapi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelloController {
 
@@ -14,6 +17,16 @@ public class HelloController {
     @GetMapping("/user")
     public User getUser(){
         return new User("谢晨", 57, "IT工程师");
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        List<User> users = new ArrayList<>();
+        users.add(new User("谢晨",57,"工程师"));
+        users.add(new User("田中", 30, "Java开发者"));
+        users.add(new User("山田", 25, "前端工程师"));
+
+        return users;
     }
 
 }
