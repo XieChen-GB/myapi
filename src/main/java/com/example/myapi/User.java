0bb041b7c702ebd6154ecf805833bc24c23b028a
XieChen-GB <xieche@gmail.com>
2026-05-30 16:48:59 +0900

    Spring Boot：User Entity类

--- src/main/java/com/example/myapi/User.java
+++ src/main/java/com/example/myapi/User.java
@@ -1,15 +1,28 @@
 package com.example.myapi;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+
 public class User {
+
+    @Id
+    private String userId;
+
     private String name;
     private int age;
     private String job;
 
-    public User(String name, int age, String job) {
+    public User(){}
+
+    public User(String userId, String name, int age, String job) {
+        this.userId = userId;
         this.name = name;
         this.age = age;
         this.job = job;
     }
 
+    public String getUserId() { return userId; }
     public String getName() { return name; }
     public int getAge() { return age; }
     public String getJob() { return job; }
